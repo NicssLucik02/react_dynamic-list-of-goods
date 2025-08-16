dda24c784bcaf765d075720d9257f577f3eb8096
nikita.lucenko02@gmail.com <nikitalucenko@MacBook-Air-Nikita.local>
2025-08-16 11:45:00 +0300

    dynamic-list-of-goods solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
-import React from 'react';
 import './App.scss';
+
+import React, { useEffect, useState } from 'react';
 import { GoodsList } from './GoodsList';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+enum ListType {
+  Nothing = '',
+  All = 'All',
+  Five = 'Five',
+  Red = 'Red',
+}
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [changeList, setChangeList] = useState<ListType>(ListType.Nothing);
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    if (changeList === ListType.Nothing) {
+      return;
+    }
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    let items: Promise<Good[]> | undefined;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    if (changeList === ListType.All) {
+      items = getAll();
+    } else if (changeList === ListType.Five) {
+      items = get5First();
+    } else if (changeList === ListType.Red) {
+      items = getRedGoods();
+    }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    items?.then((data: Good[]) => setGoods(data));
+  }, [changeList]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setChangeList(ListType.All)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setChangeList(ListType.Five)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setChangeList(ListType.Red)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
